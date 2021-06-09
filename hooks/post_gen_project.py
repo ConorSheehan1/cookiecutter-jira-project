3916aa689403159fd3f0a2688a2617a114de4770
Conor Sheehan <conor.sheehan.dev@gmail.com>
2021-06-09 18:30:49 +0100

    closes #11
    add basic git hook to check commit message includes project jira code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,5 +5,9 @@ repo_dir = '{{cookiecutter.repo_dir}}'
 project_path = f'{base_dir}/{project_name}'
 
 print(f'\033[0;32mSuccessfully generated {project_path}\033[0m')
-print('To load scripts run the following:')
-print(f'\techo "load {project_path}/utils.sh" >> ~/.bashrc')
+print('\033[0;33mTo make the utility functions available in your shell, source utils.sh. e.g.\033[0m')
+print(f'\techo "source {project_path}/utils.sh" >> ~/.bashrc')
+
+print()
+print('\033[0;33mTo setup git hooks run the following:\033[0m')
+print(f'\tln -s {project_path}/githooks/commit-msg {repo_dir}/.git/hooks/commit-msg')
